f0f7f699dfb7d36b1adcbf5581cae037c1504ee4
JessicaGrisales <pi43jro@eduvaud.ch>
2025-12-04 00:19:40 +0100

    feat(server):Création de l'import et charger dotenv

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,5 +1,31 @@
+// 1. Pour la base de données (tu as créé db.js à la racine)
+const { register } = require("module");
+const db = require("../db");
+
+// 2. Le module natif de Node.js pour le hachage sécurisé
+const crypto = require("crypto");
+
 module.exports = {
-    get: (req, res) => {
-        res.send("User: Sarah Test");
-    }
+  get: (req, res) => {
+    res.send("User: Sarah Test");
+  },
+  //Nouvelle fonction d'enregistrement
+  register: (req, res) => {
+    // 1. Récupérer les données de l'utilisateur (username, password, etc.)
+    const { username, password, firstname, lastname } = req.body;
+    // 2. Créer un sel unique et sécurisé (salt)
+    const salt = crypto.randomBytes(16).toString("hex");
+    // 16 octets génèrent une chaîne hexadécimale de 32 caractères
+
+    // crypto.pbkdf2Sync(mot_de_passe, sel, iterations, longueur_cle, algorithme)
+    const hash = crypto
+      .pbkdf2Sync(
+        password,
+        salt,
+        1000, // Nombre d'itérations
+        64, // Longueur de la clé dérivée (en octets)
+        "sha512" // Algorithme de hachage
+      )
+      .toString("hex");
+  },
 };
--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
+// 1. Importer et charger dotenv en premier
+require("dotenv").config();
+
 const express = require("express");
 
-
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
-
-
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
 // Démarrage du serveur
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on port 8080");
 });
