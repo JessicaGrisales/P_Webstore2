c639e9579a9d11f63f2e7169a09d859cf1091958
JessicaGrisales <pi43jro@eduvaud.ch>
2025-12-04 11:08:51 +0100

    feat(login):Création de la route Post

--- app/routes/User.js
+++ app/routes/User.js
@@ -8,6 +8,9 @@ const UserController = require("../controllers/UserController");
 // 2. Définir la route POST /register pour l'enregistrement
 // Cette route appelle la fonction register qui a été codée dans le contrôleur.
 router.post("/register", userController.register);
+
+// Définir la route POST Login
+router.post("/login", userController.login);
 // Route déjà existante
 router.get("/", controller.get);
 
